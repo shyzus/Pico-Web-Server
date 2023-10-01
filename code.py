3bd6b0038676e758a3eac3b153db0830fa4d008d
Shyzus <dev@awaly.nl>
2023-10-01 20:01:24 +0000

    Added shutdown procedure

--- code.py
+++ code.py
@@ -13,11 +13,9 @@ import staticWSGIApplication as StaticWSGIApplication
 import adafruit_requests as requests
 from adafruit_esp32spi import adafruit_esp32spi
 import adafruit_esp32spi.adafruit_esp32spi_wifimanager as wifimanager
-
 import adafruit_wsgi.esp32spi_wsgiserver as server
-from adafruit_wsgi.wsgi_app import WSGIApp
-
 import adafruit_logging as logging
+from adafruit_ticks import ticks_ms, ticks_add, ticks_less
 
 LICENSE_PATH = "/sd/LICENSE"
 SECRETS_PATH = "/sd/secrets.json"
@@ -25,9 +23,12 @@ SECRETS = None
 SSID = "ESP32"
 PSK = "ESP32PICO"
 
+a_pin = digitalio.DigitalInOut(board.GP12)
+a_pin.direction = digitalio.Direction.INPUT
+a_pin.pull = digitalio.Pull.UP
+
 spi = busio.SPI(board.GP18, board.GP19, board.GP16)
 cs = digitalio.DigitalInOut(board.GP22)
-
 sdcard = adafruit_sdcard.SDCard(spi, cs)
 vfs = storage.VfsFat(sdcard)
 storage.mount(vfs, "/sd")
@@ -65,12 +66,10 @@ try:
             PSK = SECRETS["PSK"]
         except ValueError as e:
             logger.error(f"Malformed secrets! {e}")
-            fileHandler.close()
-            sys.exit()
+            shutdown_procedure()
         except KeyError as e:
             logger.error(f"Failed to find key in secrets! Key: {e}")
-            fileHandler.close()
-            sys.exit()
+            shutdown_procedure()
 except OSError:
     logger.error(f"{SECRETS_PATH} ,not found! Using default SSID/PSK to setup AP!")
     logger.info(f"SSID: {SSID}, PSK: {PSK}")
@@ -91,15 +90,36 @@ server.set_interface(esp)
 wsgiServer = server.WSGIServer(80, application=web_app, debug=True)
 wsgiServer.start()
 
+def a_button_pressed():
+    return not a_pin.value
+
+def shutdown_procedure():
+    logger.info("Shutting down...")
+    fileHandler.close()
+    storage.umount(vfs)
+    sys.exit()
+
 while True:
     try:
         fileHandler.stream.flush()
+        shutdown = False
+        if a_button_pressed():
+            deadline = ticks_add(ticks_ms(), 5000)
+            while ticks_less(ticks_ms(), deadline):
+                if not a_button_pressed():
+                    shutdown = False
+                    break
+                else:
+                    shutdown = True
+        if shutdown:
+            shutdown_procedure()
+            
         wsgiServer.update_poll()
         led.value=False
         # Could do any other background tasks here, like reading sensors
     except (ValueError, RuntimeError) as e:
         logger.error(f"Failed to update server, restarting ESP32\n {e}")
-        filehHandler.close()
+        fileHandler.close()
         storage.umount(vfs)
         wifi.reset()
         continue
